84e6ab7923b49a0ce89287ed7689679cf37d655b
sefika86 <sefikaakin86@gmail.com>
2023-02-20 23:39:41 -0500

    create Mother Board Factory

---
+++ spring-00-Coupling/src/main/java/com/cydeo/loosely_coupled/Main.java
@@ -0,0 +1,21 @@
+package com.cydeo.loosely_coupled;
+
+import com.cydeo.loosely_coupled.caseFactory.Case;
+import com.cydeo.loosely_coupled.caseFactory.DellCase;
+import com.cydeo.loosely_coupled.monitorFactory.AcerMonitor;
+import com.cydeo.loosely_coupled.monitorFactory.Monitor;
+import com.cydeo.loosely_coupled.motherBoardFactory.AsusMotherBoard;
+import com.cydeo.loosely_coupled.motherBoardFactory.MotherBoard;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Monitor theMonitor = new AcerMonitor("27inch Beast", "Acer", 27);
+        Case theCase = new DellCase("220B", "Dell", "240");
+        MotherBoard theMotherboard = new AsusMotherBoard ("BJ-200", "Asus", 4, 6, "v2.44");
+
+        PC myPc = new PC(theCase,theMonitor,theMotherboard);
+
+        myPc.powerUp();
+    }
+}
