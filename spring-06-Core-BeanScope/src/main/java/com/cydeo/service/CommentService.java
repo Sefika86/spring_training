576e1c6dc4d36a3c8eb3eca0c9dff2910f81984c
sefika86 <sefikaakin86@gmail.com>
2023-02-26 05:39:21 -0500

    BeanScope

---
+++ spring-06-Core-BeanScope/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,34 @@
+package com.cydeo.service;
+
+import com.cydeo.model.Comment;
+import com.cydeo.proxy.CommentNotificationProxy;
+import com.cydeo.repository.CommentRepository;
+import org.springframework.beans.factory.BeanDefinitionStoreException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Scope (BeanDefinition.SCOPE_PROTOTYPE) //we will not use it, Spring use the default approach
+                                       //which is Singleton approach but good to know this Prototype approach which gives you a new object all the time
+
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
+
+    @Autowired //optional: Spring already takes care the injection(wiring) for you
+    public CommentService(CommentRepository commentRepository, @Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    public void publishComment(Comment comment){
+        commentRepository.storeComment(comment);  //save in the DB
+        commentNotificationProxy.sendComment(comment);  //send email
+    }
+
+}
