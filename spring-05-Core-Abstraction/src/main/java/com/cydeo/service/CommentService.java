0e33213c542b70ab541e2872a7a45881949bd26f
sefika86 <sefikaakin86@gmail.com>
2023-02-26 04:14:01 -0500

    abstraction

---
+++ spring-05-Core-Abstraction/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,19 @@
+package com.cydeo.service;
+
+import com.cydeo.model.Comment;
+import com.cydeo.proxy.CommentNotificationProxy;
+import com.cydeo.repository.CommentRepository;
+import com.cydeo.repository.DBCommentRepository;
+
+public class CommentService {
+
+    private CommentRepository commentRepository;
+    private CommentNotificationProxy commentNotificationProxy;
+
+
+    public void publishComment(Comment comment){
+        commentRepository.storeComment(comment);  //save in the DB
+        commentNotificationProxy.sendComment(comment);  //send email
+    }
+
+}
